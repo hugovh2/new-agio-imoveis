9c4904aac72e9acba4981c70902b401758a85576
Coq <vitorhugo1524@gmail.com>
2025-02-27 01:50:37 -0300

    Adicionando foto de perfil

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    domains: ['res.cloudinary.com'], // Permite carregar imagens do Cloudinary
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
