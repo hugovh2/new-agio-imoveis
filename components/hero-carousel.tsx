cc3d83df83a3ead9d4b025bb9fa64cbefc90346e
Coq <vitorhugo1524@gmail.com>
2025-03-11 00:04:28 -0300

    alterando title

--- components/hero-carousel.tsx
+++ components/hero-carousel.tsx
@@ -11,7 +11,7 @@ const slides = [
   {
     image:
       "https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80",
-    title: "Encontre o Imóvel dos Seus Sonhos",
+    title: "Agio Imóveis", // Título alterado
     subtitle: "As melhores oportunidades de ágio em um só lugar",
   },
   {
